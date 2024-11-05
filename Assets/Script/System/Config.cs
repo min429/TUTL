2736de8348f4b38484fe59f33ae044eab30d5eb5
min429 <lsm990429@gmail.com>
2024-11-05 15:56:55 +0900

    feat: api 요청 url을 변경한다.

--- Assets/Script/System/Config.cs
+++ Assets/Script/System/Config.cs
@@ -1,4 +1,4 @@
 public static class Config
 {
-    public static readonly string baseUrl = "http://localhost:8080/api/";
+    public static readonly string baseUrl = "http://192.168.0.2:8080/api/";
 }
